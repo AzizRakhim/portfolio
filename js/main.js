ad123913cd5e3f4bf7063a08b390ae683327ef83
Aziz_Rakim <azizbekrahimjonov80@gmail.com>
2022-06-12 16:42:21 +0500

    corrected the colors change

--- js/main.js
+++ js/main.js
@@ -41,3 +41,90 @@ scrollNavbar = () => {
     });
   }
 }
+
+// NAV COLOR CHANGE
+
+let elNavItems = document.querySelectorAll(".header__link");
+
+elNavItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    elNavItems.forEach((el) => {
+      el.classList.remove("active");
+    })
+    item.classList.add("active");
+  })
+})
+
+// PORTFOLIO FILTER
+
+let elBtns = document.querySelectorAll(".work__list li");
+let elPortfolioList = document.querySelector(".work__example-list");
+
+clickedMe("react");
+
+function clickedMe(type){
+  elPortfolioList.innerText = ""
+  portfolioObj.forEach((item) => {
+    if(item.type == type.toLowerCase()){
+      let elItem = document.createElement("li");
+      elItem.className = "work__example-item col-12 col-lg-4";
+  
+      elItem.innerHTML = `
+        <div class="work__item-inner">
+          <div class="work__bg-holder">
+            <div class="work__bimg">
+              <img src="${item.img}" alt"${elItem.name}" />
+            </div>
+          </div>
+          <div class="work__whole-color"></div>
+          <div class="work__icon">
+            <i class='${item.react}'></i>
+          </div>
+          <div class="work__details">
+            <span class="work__name">
+              ${item.name}
+            </span>
+            <div class="work__link-holder d-flex align-items-center">
+              <a class="work__external" href="${item.gitHub}" target="_blank">
+                <i class='bx bxl-github'></i>
+              </a>
+              <a class="work__external" href="${item.netlify}" target="_blank">
+                <i class='bx bxl-netlify'></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      `;
+  
+      elPortfolioList.appendChild(elItem);
+    }
+  })
+}
+
+elBtns.forEach((item) => {
+  item.addEventListener("click", () => clickedMe(item.innerText));
+});
+
+// COLOR SETTING BTN
+
+let elSetting = document.querySelector(".color__icon-box");
+let num = 0;
+
+elSetting.addEventListener("click", () => {
+  if(num == 0){
+    elSetting.parentNode.style.right = "0px";
+    num++;
+  } else {
+    elSetting.parentElement.style.right = "-200px";
+    num = 0;
+  }
+});
+
+let elColorBtns = document.querySelectorAll(".color__btn");
+
+elColorBtns.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    document.body.className = "";
+    document.body.classList.add(item.classList[1]);
+  })
+})
---
+++ js/portfolioObj.js
@@ -0,0 +1,65 @@
+let portfolioObj = [
+  {
+    id : 1,
+    name : "Jaegar Resto",
+    img : "../media/images/jaegarResto.png",
+    react : "bx bxl-react",
+    type : "react",
+    gitHub : "https://github.com/AzizRakhim/jaegarResto.git",
+    netlify : "https://spectacular-pegasus-ddbdbc.netlify.app/"
+  },
+  {
+    id : 2,
+    name : "Medium",
+    img : "../media/images/medium.png",
+    react : "bx bxl-react",
+    type : "react",
+    gitHub : "https://github.com/AzizRakhim/Medium.git",
+    netlify : "https://medium-aziz-rakhim.netlify.app/"
+  },
+  {
+    id : 3,
+    name : "Badiiyat",
+    img : "../media/images/badiiyat.png",
+    react : "bx bxl-react",
+    type : "react",
+    gitHub : "https://github.com/AzizRakhim/badiiyatNew.git",
+    netlify : "https://badiiyat-finish-aziz-rakhim.netlify.app/"
+  },
+  {
+    id : 4,
+    name : "React Meals",
+    img : "../media/images/react-meals.png",
+    react : "bx bxl-react",
+    type : "react",
+    gitHub : "https://github.com/AzizRakhim/ReactMeals.git",
+    netlify : "https://reactmeals-react-aziz-rakhim.netlify.app/"
+  },
+  {
+    id : 5,
+    name : "Hamburger",
+    img : "../media/images/hamburger.png",
+    react : "bx bxl-react",
+    type : "react",
+    gitHub : "https://github.com/AzizRakhim/Hamburger.git",
+    netlify : "https://hamburger-aziz-rakhim.netlify.app/"
+  },
+  {
+    id : 6,
+    name : "New Employee",
+    img : "../media/images/new-employee.png",
+    react : "bx bxl-javascript",
+    type : "javascript",
+    gitHub : "https://github.com/AzizRakhim/new-employee.git",
+    netlify : "https://new-employee-aziz-rakhim.netlify.app/"
+  },
+  {
+    id : 7,
+    name : "Chess Kit",
+    img : "../media/images/chess-kit.png",
+    react : "bx bxl-javascript",
+    type : "javascript",
+    gitHub : "https://github.com/AzizRakhim/chess-kit.git",
+    netlify : "https://chess-kit-aziz-rakhim.netlify.app/"
+  }
+];
